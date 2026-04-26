b289760f34f11385904eb9b1c2b4bcc1ef4663a7
Niti Utami <78550802+ni-tami@users.noreply.github.com>
2026-04-26 17:21:01 +0700

    Feat/interactivity (#2)
    * min playable gem
    
    * fix

--- game/grid.js
+++ game/grid.js
@@ -1,8 +1,9 @@
 import { Cell, CellValues } from "./cell.js";
 import { getRandomInt } from "../utils/index.js";
+import { GameState } from "./state.js";
 
 export class Grid {
-    constructor(xSize, ySize, bombCount, bombLocs = []) {
+    constructor(xSize, ySize, bombCount, bombLocs = [], revealedCount = 0) {
         if (xSize < 1 | ySize < 1) {
             throw new Error("Invalid grid dimension.");
         }
@@ -14,6 +15,8 @@ export class Grid {
         this.bombCount = bombCount
         this.initEmptyGrid()
         this.bombLocs = bombLocs
+        this.revealedCount = revealedCount
+        this.gameState = GameState.INPROGRESS
     }
 
     getCellNeighborLocs(x, y) {
@@ -42,7 +45,7 @@ export class Grid {
         for (let x = 0; x < this.xSize; x++) {
             var row = []
             for (let y = 0; y < this.ySize; y++) {
-                row.push(new Cell(x, y, 0))
+                row.push(new Cell(x, y, 0, false))
             }
             this.grid.push(row);            
         }
@@ -50,15 +53,19 @@ export class Grid {
 
     initGrid() {
         var gridFlatSize = this.xSize * this.ySize
-        var bombIndexes = []
-        for (let b = 0; b < this.bombCount; b++) {
+        for (let b = 0; b < this.bombCount; b = this.bombLocs.length) {
             let bombFlatIndex = getRandomInt(0, gridFlatSize)
             let x = Math.floor(bombFlatIndex / this.xSize)
             let y = Math.floor(bombFlatIndex % this.ySize)
-            bombIndexes.push([x,y])
+            var res = this.bombLocs.find(e => e[0] == x && e[1] == y)
+            if (res) {
+                console.log("duplicate bombLoc");
+                continue;
+            }
+            this.bombLocs.push([x,y])
             this.grid[x][y].value = CellValues.BOMB;
         }
-        for (let _loc of bombIndexes) {
+        for (let _loc of this.bombLocs) {
             let neiLocs = this.getCellNeighborLocs(_loc[0], _loc[1])
             for (let neiLoc of neiLocs) {
                 if (this.grid[neiLoc[0]][neiLoc[1]].value !== CellValues.BOMB) {
@@ -73,7 +80,6 @@ export class Grid {
                 }
             }
         }
-        this.bombLocs = bombIndexes;
     }
 
     initGridWithBombLocs() {
@@ -100,6 +106,30 @@ export class Grid {
                 }
             }
         }
+    }
+
+    flood(x, y) {
+        this.grid[x][y].isRevealed = true
+        if (this.grid[x][y].value == CellValues["BOMB"]) {
+            this.gameState = GameState.LOSE;
+            return;
+        }
+        this.revealedCount += 1;
+        if (this.grid[x][y].value != CellValues[0]) {
+            return;
+        }
+        var neiLocs = this.getCellNeighborLocs(x, y);
+        if (neiLocs.length == 0) {
+            return
+        }
+        neiLocs.forEach((neiLoc) => {
+            var neiX = neiLoc[0];
+            var neiY = neiLoc[1];
+            var neiIsRevealed = this.grid[neiX][neiY].isRevealed
+            if (!neiIsRevealed) {
+                this.flood(neiX, neiY);
+            }
+        })
     }
 
     getGrid() {
--- game/index.js
+++ game/index.js
@@ -1,6 +1,7 @@
 import { Grid } from "./grid.js";
 import { Cell, CellValues } from "./cell.js";
 import { getRandomInt } from "../utils/index.js";
+import { GameState } from "./state.js";
 
 // Declare the counter
 var grid = new Grid(10, 10, 15);
@@ -12,26 +13,38 @@ const msgEl = document.getElementById("message");
 const btnReset = document.getElementById("btnReset");
 const btnSave = document.getElementById("btnSave");
 const btnLoad = document.getElementById("btnLoad");
-
+// const gridBtns = document.getElementsByClassName("cell");
+ 
 
 btnReset.addEventListener("click", resetGrid);
 btnSave.addEventListener("click", saveGrid);
 btnLoad.addEventListener("click", loadGrid);
 
+
+
 // Function to display the counter
 function updateGrid() {
   const gridEl = document.getElementById("grid");
+  gridEl.innerHTML = "";
   grid.getGrid().forEach((row) => {
     var rowEl = document.createElement("tr");
     row.forEach((cell) => {
       var tdEl = document.createElement("td");
       var btnEl = document.createElement("button");
+      btnEl.className = cell.isRevealed ? "revealed" : "covered";
+      btnEl.addEventListener("click", (e) => revealCell(e, cell));
       tdEl.appendChild(btnEl);
       btnEl.innerHTML = cell.getDescription();
       rowEl.appendChild(tdEl);  
     });
     gridEl.appendChild(rowEl);
   });
+  if (grid.gameState == GameState.WON || grid.gameState == GameState.LOSE) {
+    showMessage(grid.gameState);
+    setTimeout(function () {
+      resetGrid();
+    }, 5000);
+  }
 }
 
 // Function to diplay message
@@ -44,8 +57,6 @@ function showMessage(text) {
 
 // Function to reset the counter
 function resetGrid() {
-  const gridEl = document.getElementById("grid");
-  gridEl.innerHTML = "";
   grid = new Grid(10, 10, 15);
   grid.initGrid();
   updateGrid();
@@ -62,16 +73,23 @@ function saveGrid() {
   showMessage("Saved!");
 }
 
-// Function to load the grid
+// Function to load the grid, not including game state yet
 function loadGrid() {
-  const gridEl = document.getElementById("grid");
-  gridEl.innerHTML = "";
   let saved = localStorage.getItem("grid");
   if (saved !== null) {
     var parsed = JSON.parse(saved);
     grid = new Grid(parsed.xSize, parsed.ySize, parsed.bombCount, parsed.bombLocs);
     grid.initGridWithBombLocs();
     showMessage("Loaded!");
+  }
+  updateGrid();
+}
+
+function revealCell(e, cCell) {
+  grid.flood(cCell.x, cCell.y);
+  if (grid.revealedCount == grid.xSize * grid.ySize - grid.bombCount) {
+    grid.gameState = GameState.WON;
+    return;
   }
   updateGrid();
 }
